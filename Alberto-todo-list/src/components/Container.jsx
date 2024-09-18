ee5cfcf2c3cf42b74dbc8e51fdf193748d3f0542
Manuel Artero Anguita <manutero.developer@gmail.com>
2024-09-18 20:12:38 +0200

    suggested refactors

--- Alberto-todo-list/src/components/Container.jsx
+++ Alberto-todo-list/src/components/Container.jsx
@@ -1,6 +1,4 @@
-/* Declaracion de dominios*/
-import React, { useState } from "react";
-
+import { useState } from "react";
 
 /*Importar los componentes que se van a utilizar en el componente actual
 En este caso se utilizan los componentes principales del Check list para
@@ -10,7 +8,6 @@ import TaskList from "./TaskList";
 import FormTodo from "./FormTodo";
 
 const TodoList = () => {
-
   /* Se crea una variable de estado (Hook) en donde iremos guardando todas las actividades
   que el usuario desea realizar, al utilizar estados en reac se generan dos variables
     list -> listado de actividades
@@ -18,21 +15,41 @@ const TodoList = () => {
   */
   const [list, setList] = useState([]);
 
- /* handleAddItem Funcion para actualizar los valores en la lista
+  /* handleAddItem Funcion para actualizar los valores en la lista
     considerando las acciones que se ejecutan en FormTodo
     Los valores se le dan como estados de entrada a el TaskList
  */
 
-  const handleAddItem = addItem => {
+  const handleAddItem = (addItem) => {
     setList([...list, addItem]);
+  };
+
+  /** Cambiar a realizado el es estado de la actividad cuando se chequee*/
+  const onChangeStatus = (item) => {
+    const updatedList = list.map((listItem) => {
+      if (listItem.id === item.id) {
+        return { ...listItem, done: !listItem.done };
+      }
+      return listItem;
+    });
+    setList(updatedList);
+  };
+
+  /** remover todos los items chequeados con el filtro */
+  const onClickRemoveItem = () => {
+    const updateList = list.filter((item) => !item.done);
+    setList(updateList);
   };
 
   return (
     <div>
-
       <FormTodo handleAddItem={handleAddItem} />
 
-      <TaskList list={list} setList={setList} />
+      <TaskList
+        list={list}
+        onChangeItem={onChangeStatus}
+        onCleanUpCompletedTasks={onClickRemoveItem}
+      />
     </div>
   );
 };
--- Alberto-todo-list/src/components/TaskList.jsx
+++ Alberto-todo-list/src/components/TaskList.jsx
@@ -1,38 +1,21 @@
 import Checkbox from "./Activity";
 
-const TaskList = props => {
-  /** la entrada al TaskList es la lista y el estado de la misma */
-  const { list, setList } = props;
-
- /** Cambiar a realizado el es estado de la actividad cuando se chequee*/
-  const onChangeStatus = e => {
-     const { name, checked } = e.target;
-         const updateList = list.map(item => ({
-         ...item,
-          done: item.id === name ? checked : item.done
-       }));
-     setList(updateList);
-  };
-
-  /** remover todos los items chequeados con el filtro */
-  const onClickRemoveItem = e => {
-    const updateList = list.filter(item => !item.done);
-    setList(updateList);
-  };
+const TaskList = (props) => {
+  /** la entrada al TaskList es la lista y las funciones para que el padre pueda actualizar la lista */
+  const { list, onChangeItem, onCleanUpCompletedTasks } = props;
 
   /** Funcion que identifica el numero de elementos y pinta la lista*/
-  const chk = list.map(item => (
-    <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
+  const chk = list.map((item) => (
+    <Checkbox key={item.id} data={item} onChange={() => onChangeItem(item)} />
   ));
 
   return (
     <div className="Todo-list">
-
       {list.length ? chk : "No tasks"}
 
       {list.length ? (
         <p>
-          <button className="button-delete" onClick={onClickRemoveItem}>
+          <button className="button-delete" onClick={onCleanUpCompletedTasks}>
             Limpiar Ejecutados
           </button>
         </p>
